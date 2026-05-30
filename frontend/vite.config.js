e0b7b3a20b02d1e9d06cd31b81406fb4d1c64ff8
Jvilo <vilo.johanna@gmail.com>
2026-05-30 12:39:45 +0300

    Add phaser and db for Openshift, configutations for Openshift and local dockercompose

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://backend:3001',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false
       }
