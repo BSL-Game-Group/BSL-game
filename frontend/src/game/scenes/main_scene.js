319ea67e0ba94136dce1ea3a360edf172b71673d
JM314155i <muurimakijere4@gmail.com>
2026-07-07 02:55:11 +0300

    feat(info-desk): preload the info-desk image

--- frontend/src/game/scenes/main_scene.js
+++ frontend/src/game/scenes/main_scene.js
@@ -95,6 +95,7 @@ class MainScene extends Phaser.Scene {
         this.load.image('bsl4_room', 'assets/rooms/BSL-4 ver. 2.png');
         this.load.image('air_systems', 'assets/rooms/air-systems.jpeg');
         this.load.image('dressing_room', 'assets/rooms/dressing-room.png');
+        this.load.image('info_desk', 'assets/rooms/info-desk.png');
     }
 
     createWoodFloor() {
