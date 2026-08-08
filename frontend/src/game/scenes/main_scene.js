2737e0e5e06a3ef54b96a9ce95df6c22f007d18c
Samuli Laiho <samuli.laiho@helsinki.fi>
2026-08-08 09:27:32 +0300

    Refactor environment and map generation

--- frontend/src/game/scenes/main_scene.js
+++ frontend/src/game/scenes/main_scene.js
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 import { createRooms } from './rooms';
 import microbeService from '../../services/microbes';
 import { generateSessionId, notifyRoomEntry } from '../services/tracking';
+import { createWoodFloor, createLabFloor } from '../environment/EnvironmentBuilder';
 import { EventBus } from '../EventBus';
 import DoorGroup from '../groups/DoorGroup.js';
 import { loadSavedGame, patchSavedGame, savePlayerPosition } from '../../state/savedGame';
@@ -43,67 +44,6 @@ class MainScene extends Phaser.Scene {
         this.load.image('dresser', 'assets/dresser.png');
     }
 
-    createWoodFloor() {
-        const tileSize = 64;
-
-        if (!this.textures.exists('wood_tile')) {
-            const woodTex = this.textures.get('wood');
-            if (woodTex) {
-                const woodSrc = woodTex.getSourceImage();
-                if (woodSrc) {
-                    const tileTexture = this.textures.createCanvas('wood_tile', tileSize, tileSize);
-                    const ctx = tileTexture.getContext();
-                    const srcW = woodSrc.naturalWidth || woodSrc.width;
-                    const srcH = woodSrc.naturalHeight || woodSrc.height;
-                    ctx.drawImage(woodSrc, 0, 0, srcW, srcH, 0, 0, tileSize, tileSize);
-                    tileTexture.refresh();
-                } else {
-                    // eslint-disable-next-line no-console
-                    console.warn('wood source image not available when creating wood_tile');
-                }
-            } else {
-                // eslint-disable-next-line no-console
-                console.warn('wood texture not found when creating wood_tile');
-            }
-        }
-
-        const mapWidth = Math.ceil(this.playArea.width / tileSize);
-        const mapHeight = Math.ceil(this.playArea.height / tileSize);
-
-        const map = this.make.tilemap({
-            width: mapWidth,
-            height: mapHeight,
-            tileWidth: tileSize,
-            tileHeight: tileSize
-        });
-
-        const tileset = map.addTilesetImage('wood_tile', 'wood_tile', tileSize, tileSize, 0, 0);
-
-        const layer = map.createBlankLayer(
-            'wood_floor_layer',
-            tileset,
-            this.playArea.x,
-            this.playArea.y
-        );
-
-        layer.fill(0, 0, 0, mapWidth, mapHeight);
-        layer.setDepth(-10);
-    }
-
-    createLabFloor() {
-        const startX = 0;
-        const width = 1280;
-        const height = 720;
-        const tileScale = 440 / 442;
-
-        const floor = this.add
-            .tileSprite(startX, 0, width, height, 'labs_floor')
-            .setOrigin(0, 0);
-        floor.tileScaleX = tileScale;
-        floor.tileScaleY = tileScale;
-        floor.setDepth(-9);
-    }
-
     create() {
         this.savedGame = loadSavedGame();
 
@@ -114,8 +54,8 @@ class MainScene extends Phaser.Scene {
         this.physics.world.setBounds(0, 0, gameWidth, gameHeight);
         this.playArea = new Phaser.Geom.Rectangle(0, 0, gameWidth, gameHeight);
 
-        this.createWoodFloor();
-        this.createLabFloor();
+        createWoodFloor(this);
+        createLabFloor(this);
 
         if (!window.__gameData?.sessionId) {
             const sessionId = this.savedGame?.sessionId ?? generateSessionId(); // Removed 'this.'
